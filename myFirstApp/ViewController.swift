c485b04661fd7a2d690039fb145984de72be3174
Guest User <Guest@imac-18.local>
2020-11-27 10:38:40 +0100

    Initial Commit

---
+++ myFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myFirstApp
+//
+//  Created by Guest User on 27.11.2020.
+//  Copyright © 2020 Guest User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
