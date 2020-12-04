27076989cb8b960d7a38a86f48a7bd50994593e4
Guest User <Guest@imac-18.local>
2020-12-04 07:16:33 +0100

    init

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.1'
+
+target 'myFirstApp' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for myFirstApp
+pod 'Alamofire', '~> 4.0'
+pod 'SwiftSpinner'
+
+  target 'myFirstAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- myFirstApp/ViewController.swift
+++ myFirstApp/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftSpinner
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        SwiftSpinner.show("Downloading data");
+        let user: String? = inputText.text;
+        
+        let address = "https://api.github.com/users/" + user! + "/repos"
+        Alamofire.request(address).responseString {  response in
+            switch response.result{
+            case .success(let JSON):
+                self.resultLabel.text = JSON
+                print(JSON)
+            case .failure(_):
+                self.resultLabel.text = "Error"
+            }
+        }
+        
+        SwiftSpinner.hide()
+        
+    }
+    
 }
+
 
